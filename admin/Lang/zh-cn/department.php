89747e4bee6da0cc701a9aa9385afdc71978fa52
mapengsheng <798431291@qq.com>
2015-05-20 19:25:31 +0800

    xiaoma5.20

---
+++ admin/Lang/zh-cn/department.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'NAME' => '部门',
+
+	'intro'=>'简介',
+	'img'=>'部门logo',
+	'num'=>'部门编号',
+	'contract'=>'联系方式',
+	'office_place'=>'办公地点',
+	'sort_order'=>'排序值',
+	'OPERATIONAL'=>'操作'
+);
+?>
---
+++ admin/Lang/zh-cn/teacher.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'NAME' => '名称',    
+	'sort_order'=>'排序值',
+	'PLACE'=>'办公地点',
+	'PHONE'=>'办公电话',
+	'create_time'=>'增加时间',
+	'position'=>'职位',
+	'intro'=>'个人简历',
+	'img'=>'头像',
+	'type'=>'教师/领导',//0教师；1领导；
+	'ishot'=>'是否名师',//0不是名师；1教学名师；
+	'ORDID'=>'排序',
+	'IS_HOT'=>'热门推荐',
+	'OPERATIONAL'=>'操作'
+);
+?>
