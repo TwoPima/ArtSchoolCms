607fe907dcde290354517f05aff7a92faa40d71e
mapengsheng <798431291@qq.com>
2015-05-26 17:47:13 +0800

    index

--- index/Lib/Action/MasterAction.class.php
+++ index/Lib/Action/MasterAction.class.php
@@ -5,8 +5,12 @@
 */
 class MasterAction extends CommonAction {
 	public function _before_index(){
+		//加载头部导航信息
 		$mod_cate_list=M('Master_cate');
 		$re_cate_list=$mod_cate_list->where('pid=0')->order('sort_order ASC')->select();
+		$this->assign('cate_list',$re_cate_list);
+	}
+		public function index(){
 		//资讯列表
 		$detail_mod=M('Master');
 		$where1['status']="1";
@@ -18,8 +22,19 @@ class MasterAction extends CommonAction {
 		$showPage = $page->show();
 		$this->assign('master_article_list',$article_list);
 		$this->assign("page", $showPage);
-		$this->assign('cate_list',$re_cate_list);
+		//上一篇
+		$front=$detail_mod->where("id<".$_GET['id'])->order('id desc')->limit('1')->find();
+		if (empty($front)) {
+			$front="没有了！";
+		}
+		//下一篇
+		$after=$detail_mod->where("id>".$_GET['id'])->order('id desc')->limit('1')->find();
+		if (empty($after)) {
+			$after="没有了！";
+		}
+		$this->display();
 	}
+	
 	public function _before_detail(){
 		$mod_cate_list=M('Master_cate');
 		$re_cate_list=$mod_cate_list->where('pid=0')->order('sort_order ASC')->select();
@@ -33,30 +48,24 @@ class MasterAction extends CommonAction {
 		//上一篇
 		$front=$model->where("id<".$_GET['id'])->order('id desc')->limit('1')->find();
 		if (empty($front)) {
-			$front="没有了！";
+			$front="3123123123123";
 		}
 		//下一篇
 		$after=$model->where("id>".$_GET['id'])->order('id desc')->limit('1')->find();
 		if (empty($after)) {
-			$after="没有了！";
+			$after="132123123";
 		}
 		//分类列表
 		$menu_mod = M('Master_cate');
 		$cate_where['id']=$result_se[0]['cate_id'];
 		$cate_where['status']="1";
 		$result_cate = $menu_mod->where($cate_where)->order('sort_order ASC')->select();
-		//分类名称详细显示
-		$where2['id']=$_GET['id'];
-		$detail_cate = $menu_mod->where($where2)->select();
 	
-		$getNowHere=$this->getNowHere($result_se[0]['cate_id']);
 	
-		$this->assign('now_here',$getNowHere);
 		$this->assign('mainleft_cate_list',$result_cate);
-		$this->assign('detail_cate',$detail_cate);
 	
-		$this->assign('front',$front);
-		$this->assign('after',$after);
+		$this->assign('front',$front);//上一条
+		$this->assign('after',$after);//下一条
 		$this->assign('detail',$result_se);
 		$this->display();
 	}
